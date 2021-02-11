187180761c5aec3452f789d12002fdce0cfbbf1c
handrkanda <handrykanda@gmail.com>
2021-02-11 14:42:49 +0200

    header

---
+++ src/components/header/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+
+function Header({ country, countries, onCountryChange }) {
+  return (
+    <div className="app__header">
+      <h1>COVID-19 Tracker</h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value={country} onChange={onCountryChange}>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          {countries.map((item) => (
+            <MenuItem key={item.value} value={item.value}>
+              {item.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+export default Header;
